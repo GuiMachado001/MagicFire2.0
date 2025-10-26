e858e9d035e4ed4e28e564b13be9752fe113ad65
Guilherme Machado Neves <gui.m.neves01@gmail.com>
2025-10-26 12:23:12 -0300

    Idle

--- MagicFire2.0/Assets/Scripts/PlayerMovement.cs
+++ MagicFire2.0/Assets/Scripts/PlayerMovement.cs
@@ -1,33 +1,39 @@
 using UnityEngine;
-using UnityEngine.InputSystem; // importante para o Input System
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private PlayerControls controls;  // referência do input
+    private PlayerControls controls;
     private Vector2 moveInput;
     public float speed = 5f;
+    private Animator animator;
 
     private void Awake()
     {
         controls = new PlayerControls();
+        animator = GetComponent<Animator>();
 
+        // Movimento
         controls.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
         controls.Player.Move.canceled += ctx => moveInput = Vector2.zero;
+
+        // Ataque
+        controls.Player.Attack.performed += ctx => Attack();
     }
 
-    private void OnEnable()
-    {
-        controls.Player.Enable();
-    }
-
-    private void OnDisable()
-    {
-        controls.Player.Disable();
-    }
+    private void OnEnable() => controls.Player.Enable();
+    private void OnDisable() => controls.Player.Disable();
 
     void Update()
     {
         Vector3 movement = new Vector3(moveInput.x, 0, moveInput.y);
         transform.Translate(movement * speed * Time.deltaTime);
+
+        animator.SetFloat("Speed", movement.magnitude);
+    }
+
+    void Attack()
+    {
+        animator.SetTrigger("Attack");
     }
 }
